f4327bfd6c7d30e48806f7ce13c176619e4b24eb
etka <etomehmet1907@gmail.com>
2020-07-28 09:36:15 +0300

    did PersonalInfoUpdate.kt

--- app/src/main/java/com/example/firstproject/PersonalInfoFragment.kt
+++ app/src/main/java/com/example/firstproject/PersonalInfoFragment.kt
@@ -1,9 +1,12 @@
 package com.example.firstproject
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 
 class PersonalInfoFragment : Fragment() {
@@ -14,7 +17,13 @@ class PersonalInfoFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.personal_information_layout, container, false)
-
+        val updateBtn = view.findViewById<Button>(R.id.updateBtn)
+        updateBtn.setOnClickListener {
+            val intent = Intent(requireContext(), PersonalInfoUpdate::class.java)
+            requireActivity().startActivityForResult(intent, 101)
+        }
         return view
     }
+
+
 }
--- app/src/main/java/com/example/firstproject/PersonalInfoUpdate.kt
+++ app/src/main/java/com/example/firstproject/PersonalInfoUpdate.kt
@@ -2,10 +2,33 @@ package com.example.firstproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class PersonalInfoUpdate : AppCompatActivity() {
+    lateinit var updateBtn : Button
+    lateinit var nameText : EditText
+    lateinit var surnameText : EditText
+    lateinit var birthDayText : EditText
+    lateinit var ageText : EditText
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_personal_info_update)
+        define()
+        giveAction()
+    }
+
+    fun define(){
+        updateBtn = findViewById(R.id.updateBtn2)
+        nameText = findViewById(R.id.nameText2)
+        surnameText = findViewById(R.id.surnameText2)
+        birthDayText = findViewById(R.id.birthDayText2)
+        ageText = findViewById(R.id.ageText2)
+    }
+
+    fun giveAction(){
+        updateBtn.setOnClickListener {
+            this.finish()
+        }
     }
 }
